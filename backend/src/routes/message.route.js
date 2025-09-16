2d8b48b611a78de0c3de7e785c156e3cf3180e80
pranav <pranavsoma2912@gmail.com>
2025-09-16 16:22:35 +0530

    message route added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import { getAllContacts, getMessagesByUserId , sendMessage, getChatPartners} from '../controllers/message.controller.js';
+import { protectRoute } from '../../middleware/auth.middleware.js';
+import { rateLimit } from 'express-rate-limit';
 
 const router = express.Router();
 
-router.get('/send', (req, res) => {
-    res.send('Messages endpoint');
-})
+router.use(rateLimit,protectRoute);
+
+router.get("/contacts",  getAllContacts);
+router.get("/chats",   getChatPartners);
+router.get("/:id",  getMessagesByUserId);
+router.post("/send/:id",  sendMessage);
 
 export default router;
